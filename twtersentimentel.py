9d59ee54c8fdb6e8756891eb04c186ef4031acf7
sanketsj <40729436+sanketsj@users.noreply.github.com>
2018-11-16 15:25:12 +0530

    Create twtersentimentel.py

---
+++ twtersentimentel.py
@@ -0,0 +1,22 @@
+import tweepy
+from textblob import TextBlob as tb 
+
+consumer_key = ''
+consumer_secret = ''
+
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('#MeTooIndia')
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = tb(tweet.text)
+	print(analysis.sentiment)
+
+
